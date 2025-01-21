39a1606cf6586b2eac800f757486e56fad41755a
동섭 <sk71927170@gmail.com>
2025-01-21 13:48:48 +0900

    test용

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1 className={styles.h1}>뭉치자</h1>
+      <h1 className={styles.h1}>뭉치자2</h1>
     </div>
   );
 }
