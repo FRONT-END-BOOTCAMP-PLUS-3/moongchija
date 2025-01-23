a8d2dae6ce17c245bd92683a485b883432915b27
binwoonki <vinwoonki98@gmail.com>
2025-01-23 17:45:28 +0900

    [style] login 페이지 하단 계정생성 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import styles from "./login.module.scss";
 import LoginForm from "./_components/LoginForm";
 import LogoWithTitle from "./_components/LogoWithTitle";
+import Link from "next/link";
 
 const LoginPage = () => {
   return (
@@ -21,6 +22,11 @@ const LoginPage = () => {
           />
           카카오 로그인
         </button>
+      </div>
+
+      <div className={styles.signupInfo}>
+        <span>계정이 없으신가요?</span>
+        <Link href={"/signup"}>계정 생성</Link>
       </div>
     </div>
   );
