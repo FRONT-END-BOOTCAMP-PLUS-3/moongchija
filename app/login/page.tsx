5b77be411367407fcbe440e9617db25bc09b63d8
binwoonki <vinwoonki98@gmail.com>
2025-01-23 17:31:22 +0900

    [style] login 페이지 UI 생성

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,29 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./login.module.scss";
+import LoginForm from "./_components/LoginForm";
+import LogoWithTitle from "./_components/LogoWithTitle";
+
 const LoginPage = () => {
-  return <div> 로그인 페이지</div>;
+  return (
+    <div className={styles.container}>
+      <LogoWithTitle />
+      <LoginForm />
+      <div className={styles["kakaoLoginContainer"]}>
+        <button className={styles["kakaoLoginBtn"]}>
+          <Image
+            src={"/images/logos/kakao-logo.svg"}
+            alt="카카오 로고"
+            width={50}
+            height={50}
+            className={styles["kakaoLogo"]}
+          />
+          카카오 로그인
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
