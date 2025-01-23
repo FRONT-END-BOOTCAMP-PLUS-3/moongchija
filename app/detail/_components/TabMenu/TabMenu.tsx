a2ebfaef9834400820805bcfc0cdf89592f5a1bf
황혜인 <119389723+hwanghaein@users.noreply.github.com>
2025-01-23 16:09:52 +0900

    [style] 약속 상세 탭메뉴 컴포넌트 생성

---
+++ app/detail/_components/TabMenu/TabMenu.tsx
@@ -0,0 +1,23 @@
+import styles from "./TabMenu.module.scss";
+
+const TabMenu = () => {
+  return(
+  <div className={styles.container}>
+    
+    <div className={styles.information}>
+      <button>약속 정보</button>
+    </div>
+
+    <div className={styles.settlement}>
+      <button>정산 하기</button>
+    </div>
+
+    <div className={styles.gallery}>
+      <button>추억 공간</button>
+    </div>
+
+  </div>
+  )
+};
+
+export default TabMenu;
