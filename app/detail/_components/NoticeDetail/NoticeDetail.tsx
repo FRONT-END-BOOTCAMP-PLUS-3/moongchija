b199e3fedaca7bf30f184cbecd0a9a1984894d43
황혜인 <119389723+hwanghaein@users.noreply.github.com>
2025-01-24 12:17:13 +0900

    [style] 공지사항 추가 시 모달창 추가

--- app/detail/_components/NoticeDetail/NoticeDetail.tsx
+++ app/detail/_components/NoticeDetail/NoticeDetail.tsx
@@ -1,25 +1,56 @@
+"use client";
+
+import Modal from "@/components/modal/Modal";
 import styles from "./NoticeDetail.module.scss";
 import { FaPlus, FaEdit, FaTrash } from "react-icons/fa";
+import { useState } from "react";
+import Button from "@/components/button/Button";
 
 const NoticeDetail = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.title}>
-        <span>공지사항</span>
-        <FaPlus className={styles.plusIcon} />
-      </div>
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-      {/* 공지사항 개별 박스 */}
-      <div className={styles.wrapper}>
-        <div className={styles.box}>
-          <div>늦지마요늦지마요늦지마요</div>
-          <div className={styles.icons}>
-          <FaEdit className={styles.editIcon} />
-          <FaTrash className={styles.trashIcon} />
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <span>공지사항</span>
+          <FaPlus className={styles.plusIcon} onClick={openModal} />
+        </div>
+
+        {/* 공지사항 개별 박스 */}
+        <div className={styles.wrapper}>
+          <div className={styles.box}>
+            <div>늦지마요늦지마요늦지마요</div>
+            <div>
+              <FaEdit className={styles.editIcon} />
+              <FaTrash className={styles.trashIcon} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <div className={styles.noticeModalContainer}>
+          <div className={styles.noticeModalBox}>
+            <span className={styles.noticeTitle}>공지사항 글쓰기</span>
+              <textarea className={styles.noticeContent}   placeholder="공지사항을 작성해주세요."></textarea>
+            <div  className={styles.noticeButton}>
+            <Button
+              text="등록"
+              size="sm"
+              color="--primary-color"
+              active={true}
+              onClick={closeModal}
+             
+            />
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 };
 
