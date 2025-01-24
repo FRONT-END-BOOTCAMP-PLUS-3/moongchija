0a5e7d95014509abbee6f2c0b67b436def8bd4ee
binwoonki <vinwoonki98@gmail.com>
2025-01-24 11:00:47 +0900

    [hooks] useInput hook

--- app/signup/_hooks/useInput.ts
+++ app/signup/_hooks/useInput.ts
@@ -2,14 +2,27 @@
 
 import { useState } from "react";
 
-const useInput = (initialValue: string) => {
+const useInput = (
+  initialValue: string,
+  validate?: (value: string, additionalValue?: string) => string | null
+) => {
   const [value, setValue] = useState(initialValue);
+  const [error, setError] = useState<string | null>(null);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    additionalValue?: string
+  ) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+
+    if (validate) {
+      const validationError = validate(newValue, additionalValue);
+      setError(validationError);
+    }
   };
 
-  return { value, onChange: handleChange };
+  return { value, error, onChange: handleChange };
 };
 
 export default useInput;
