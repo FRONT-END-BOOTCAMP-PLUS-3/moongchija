2cbf90917b2829a91b249fdc3d8e884efd683c34
황혜인 <119389723+hwanghaein@users.noreply.github.com>
2025-01-27 23:35:16 +0900

    [style] 상세페이지 공지사항 수정하기 스타일링

--- app/user/appointments/[id]/components/settlement/SettlementDetail/SettlementDetail.tsx
+++ app/user/appointments/[id]/components/settlement/SettlementDetail/SettlementDetail.tsx
@@ -1,12 +1,31 @@
 "use client";
 
+import { useState } from "react";
 import Button from "@/components/button/Button";
 import styles from "./SettlementDetail.module.scss";
+import Modal from "@/components/modal/Modal";
+import { FaPlus } from "react-icons/fa6";
 
 const SettlementDetail = () => {
-  // 복사 버튼 클릭 시 알림을 띄우는 함수
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const handleCopy = () => {
-    alert("복사되었습니다"); // 복사 완료 알림
+    navigator.clipboard.writeText("1122452-1234570").then(() => {
+      alert("복사되었습니다");
+    });
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleRegister = () => {
+    alert("수정되었습니다");
+    closeModal();
   };
 
   return (
@@ -72,11 +91,8 @@ const SettlementDetail = () => {
             <div className={styles.row}>
               <span>계좌번호</span>
               <div>
-                1122452-1234570
-                <button
-                  className={styles.copyButton}
-                  onClick={handleCopy} // 클릭 시 handleCopy 함수 호출
-                >
+                123456789123456
+                <button className={styles.copyButton} onClick={handleCopy}>
                   복사
                 </button>
               </div>
@@ -100,8 +116,100 @@ const SettlementDetail = () => {
           size="sm"
           color="--primary-color"
           active={true}
+          onClick={openModal}
         />
       </div>
+
+      {/* 모달 컴포넌트 */}
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <div className={styles.modalContainer}>
+          <div className={styles.modalWrapper}>
+            <span className={styles.modalTitle}>정산 내역</span>
+            <div className={styles.modalBox}>
+              {/* 내역 목록 */}
+
+              <div className={styles.modalListContainer}>
+                <div className={styles.modalListWrapper}>
+                  <div className={styles.modalListBox}>
+                    <input
+                      className={styles.place}
+                      type="text"
+                      value="고깃집"
+                      readOnly
+                    />
+                    <input
+                      className={styles.price}
+                      type="number"
+                      value="100000"
+                      readOnly
+                    />
+                    원
+                  </div>
+
+                  <button className={styles.inputPlusButton}>
+                    <FaPlus className={styles.icon} />
+                  </button>
+                </div>
+
+                {/* 결과 */}
+                <div className={styles.modalResultBox}>
+                  <div className={styles.totalPrice}>
+                    <span>총 액</span>
+                    <div className={styles.currency}>
+                      <input type="number" value="100000" readOnly />
+                      <span>원</span>
+                    </div>
+                  </div>
+
+                  <div className={styles.totalNumber}>
+                    <span>인원 수</span>
+
+                    <div className={styles.numberInput}>
+                      <span className={styles.divide}>÷</span>
+                      <input type="number" value="4" readOnly />
+                      <span>명</span>
+                    </div>
+                  </div>
+
+                  <div className={styles.dividedPrice}>
+                    <span>인 당</span>
+                    <div className={styles.priceInput}>
+                      <input type="number" value="25000" readOnly />
+                      <span>원</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <span className={styles.modalAccountTitle}>정산 계좌 정보</span>
+              <div className={styles.modalAccountBox}>
+                <div className={styles.account}>
+                  <span>계좌 번호</span>
+                  <input type="text" value="123456789123456" readOnly />
+                </div>
+                <div className={styles.bank}>
+                  <span>은행사</span>
+                  <input type="text" value="국민은행" readOnly />
+                </div>
+                <div className={styles.depositor}>
+                  <span>예금주</span>
+                  <input type="text" value="김코난" readOnly />
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* 모달 내 수정하기 버튼 */}
+          <div className={styles.editButton}>
+            <Button
+              text="수정하기"
+              size="sm"
+              color="--primary-color"
+              active={true}
+              onClick={handleRegister}
+            />
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
