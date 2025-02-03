f8fa35d45fe1ea9adf67ad804ca3fb7cddcfec7f
binwoonki <vinwoonki98@gmail.com>
2025-02-03 14:26:44 +0900

    [feat] 로그인 실패 시 에러메시지 추가

--- application/usecases/auth/DfLoginUsecase.ts
+++ application/usecases/auth/DfLoginUsecase.ts
@@ -15,9 +15,9 @@ export class DfLoginUsecase {
       return { token, user };
     } catch (error: unknown) {
       if (error instanceof Error) {
+        console.error("로그인 실패:", error.message);
         throw new Error("로그인 실패: " + error.message);
       }
-
       throw new Error("알 수 없는 에러가 발생했습니다.");
     }
   }
