9b89c79c1b35cbf6e23b61a3b7581bc2b35fcc11
binwoonki <vinwoonki98@gmail.com>
2025-02-04 14:20:16 +0900

    [feat] 카카오 로그인 리팩토링 - 인가 코드 요청 및 액세스 토큰 발급 로직 추가 - Kakao API를 통해 사용자 정보 조회 기능 구현

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,8 +1,8 @@
-import { DfSocialLoginUseCase } from "@/application/usecases/auth/DfSocialLoginUseCase";
-import { SbAuthRepository } from "@/infrastructure/repositories/SbAuthRepository";
-import { jwtDecode } from "jwt-decode";
-
 import { NextRequest, NextResponse } from "next/server";
+import { getKakaoUserInfo } from "../kakao-user/route";
+import { SbUserRepository } from "@/infrastructure/repositories/SbUserRepository";
+import { DfSocialLoginUseCase } from "@/application/usecases/auth/DfSocialLoginUseCase";
+import { SbUserEmojiRepository } from "@/infrastructure/repositories/SbUserEmojiRepository";
 
 export async function GET(request: NextRequest) {
   try {
@@ -11,49 +11,73 @@ export async function GET(request: NextRequest) {
 
     if (!code) {
       return NextResponse.json(
-        {
-          error: "카카오 인증 코드가 없습니다.",
-        },
+        { success: false, message: "인가 코드 없음" },
         { status: 400 }
       );
     }
 
-    const authRepository = new SbAuthRepository();
-    const socialLoginUseCase = new DfSocialLoginUseCase(authRepository);
+    const tokenRes = await fetch("https://kauth.kakao.com/oauth/token", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        grant_type: "authorization_code",
+        client_id: process.env.KAKAO_CLIENT_ID!, // REST API 키
+        redirect_uri: process.env.KAKAO_REDIRECT_URI!, // 콜백 URL
+        code: code, // 인가 코드
+        client_secret: process.env.KAKAO_CLIENT_SECRET!, // (옵션) 보안 강화를 위한 secret key
+      }),
+    });
 
-    const accessToken = await socialLoginUseCase.execute(
-      "kakao",
-      code as string
+    const tokenData = await tokenRes.json();
+
+    if (!tokenRes.ok) {
+      return NextResponse.json(
+        { success: false, message: "토큰 요청 실패", error: tokenData },
+        { status: 400 }
+      );
+    }
+
+    const accessToken = tokenData.access_token;
+
+    const userRes = await fetch("https://kapi.kakao.com/v2/user/me", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+
+    const userData = await userRes.json();
+    if (!userRes.ok) {
+      return NextResponse.json(
+        { success: false, message: "사용자 정보 요청 실패", error: userData },
+        { status: 400 }
+      );
+    }
+
+    const userInfo = await getKakaoUserInfo(tokenData.access_token);
+
+    const userRepository = new SbUserRepository();
+    const emojiRepository = new SbUserEmojiRepository();
+    const socialLoginUsecase = new DfSocialLoginUseCase(
+      emojiRepository,
+      userRepository
     );
 
-    if (accessToken) {
-      const decodedToken = jwtDecode(accessToken);
+    const user = await socialLoginUsecase.execute(userInfo);
 
-      const userId = decodedToken.sub;
+    const redirectUrl = `${process.env.NEXT_PUBLIC_SITE_URL}/user/appointments`;
+    const response = NextResponse.redirect(redirectUrl);
 
-      const response = NextResponse.redirect(
-        `${process.env.NEXT_PUBLIC_SITE_URL}/user/appointments`
-      );
-
-      response.cookies.delete("sb-yswjnlalguzoxdcmydxr-auth-token.0");
-      response.cookies.delete("sb-yswjnlalguzoxdcmydxr-auth-token.1");
-
-      if (userId) {
-        response.cookies.set("userId", userId, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production", // 배포 환경에서만 secure 설정
-          sameSite: "strict",
-          maxAge: 60 * 60,
-        });
-      }
-
-      return response;
-    } else {
-      return NextResponse.json(
-        { error: "Access Token is missing or invalid" },
-        { status: 400 }
-      );
+    if (user.access_token) {
+      response.cookies.set("userId", user.access_token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 60 * 60,
+      });
     }
+    return response;
   } catch (error) {
     console.error("카카오 로그인 오류:", error);
 
