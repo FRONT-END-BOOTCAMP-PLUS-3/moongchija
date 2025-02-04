76313cea6e95ebad88f69eef688f1d09c4b1f721
binwoonki <vinwoonki98@gmail.com>
2025-02-04 14:57:14 +0900

    [refactor] 로그인 시 에러메시지 수정

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -33,18 +33,23 @@ export const POST = async (request: NextRequest) => {
 
     return response;
   } catch (error: unknown) {
-    if (
-      error instanceof Error &&
-      error.message.includes("Invalid login credentials")
-    ) {
+    if (error instanceof Error) {
+      if (error.message.includes("유효하지 않은 이메일 또는 비밀번호입니다.")) {
+        return NextResponse.json(
+          { error: "유효하지 않은 이메일 또는 비밀번호입니다." },
+          { status: 401 }
+        );
+      }
+
+      console.error("❌ 로그인 API 오류:", error.message);
       return NextResponse.json(
-        { error: "유효하지 않은 이메일 또는 비밀번호입니다." },
-        { status: 401 }
+        { error: "로그인 중 예상치 못한 오류가 발생했습니다." },
+        { status: 500 }
       );
     }
 
     return NextResponse.json(
-      { error: "알 수 없는 에러가 발생했습니다." },
+      { error: "서버 내부 오류가 발생했습니다." },
       { status: 500 }
     );
   }
