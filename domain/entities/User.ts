52ae56df30c914db9fe59f110bcee7ccdbc445b7
binwoonki <vinwoonki98@gmail.com>
2025-02-04 14:37:09 +0900

    [refactor] User 인터페이스 수정 provider, kakao_id 추가

--- domain/entities/User.ts
+++ domain/entities/User.ts
@@ -1,9 +1,10 @@
 export interface User {
   id: string;
   user_email: string;
-  password: string;
+  password: string | "";
   nickname: string;
   emoji: string;
   created_at: Date;
   provider: string;
+  kakao_id?: number;
 }
