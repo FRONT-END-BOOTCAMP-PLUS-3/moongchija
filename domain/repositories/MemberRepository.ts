6af729827783f3015c9efc1fc71f8877a5a06033
Dongseup Shim <108253922+ShimDongseup@users.noreply.github.com>
2025-02-05 19:51:03 +0900

    Revert "[feat]  투표상태에 따라 투표가능여부 결정하는 기능 추가, 시간 투표 로직 수정"

--- domain/repositories/MemberRepository.ts
+++ domain/repositories/MemberRepository.ts
@@ -1,26 +1,12 @@
-import { Member } from "./../entities/Member";
+import { Member } from './../entities/Member';
+
 
 export interface MemberRepository {
-  addMemberToAppointment(userId: string, appointmentId: number): Promise<void>; // 약속에 멤버로 추가
+    addMemberToAppointment(userId: string, appointmentId: number): Promise<void>; // 약속에 멤버로 추가
+  
+    isUserInAppointment(userId: string, appointmentId: number): Promise<boolean>; // 약속의 멤버인지 확인 (멤버는 재투표 불가)
 
-  isUserInAppointment(userId: string, appointmentId: number): Promise<boolean>; // 약속의 멤버인지 확인 (멤버는 재투표 불가)
-
-  findByUserId(userId: string): Promise<Member[]>;
-
-  findByAppointment_id(appointment_id: number): Promise<Member[]>;
-
-  getMemberStatus(
-    userId: string,
-    appointmentId: number
-  ): Promise<{
-    is_vote: boolean;
-  } | null>;
-
-  updateVoteStatus(
-    userId: string,
-    appointmentId: number,
-    isVote: boolean
-  ): Promise<void>;
-
-  getVotedMemberIdsByAppointment(appointmentId: number): Promise<string[]>;
+    findByUserId(userId: string): Promise<Member[]>
+    
+    findByAppointment_id(appointment_id: number): Promise<Member[]>
 }
