18ae0067f0c7b322abe81ca48cf327d0baebe1b9
binwoonki <vinwoonki98@gmail.com>
2025-02-06 17:46:03 +0900

    [feat] 유저 닉네임 중복 체크 DTO 정의

--- application/usecases/auth/DfUserNicknameCheckUsecase.ts
+++ application/usecases/auth/DfUserNicknameCheckUsecase.ts
@@ -1,9 +1,10 @@
 import { UserRepository } from "@/domain/repositories/UserRepository";
+import { UserNicknameCheckResponseDto } from "./dto/UserNicknameCheckResponseDto";
 
 export class DfUserNicknameCheckUsecase {
   constructor(private userRepository: UserRepository) {}
 
-  async excute(nickname: string) {
+  async excute(nickname: string): Promise<UserNicknameCheckResponseDto> {
     const existingNickname = await this.userRepository.findUserByNickname(
       nickname
     );
