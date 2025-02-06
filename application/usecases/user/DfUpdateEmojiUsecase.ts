bf6d9b80e590306274ea0bce5306531d151db59e
binwoonki <vinwoonki98@gmail.com>
2025-02-06 15:50:56 +0900

    [refactor] DfUpdateEmojiUsecase에서 emoji만 보내기로 수정

--- application/usecases/user/DfUpdateEmojiUsecase.ts
+++ application/usecases/user/DfUpdateEmojiUsecase.ts
@@ -3,10 +3,7 @@ import { SbUserRepository } from "@/infrastructure/repositories/SbUserRepository
 export class DfUpdateEmojiUsecase {
   constructor(private userRepository: SbUserRepository) {}
 
-  async execute(
-    userId: string,
-    updateData: { nickname?: string; emoji?: string }
-  ) {
+  async execute(userId: string, updateData: { emoji: string }) {
     const userInfo = await this.userRepository.updateUserInfo(
       userId,
       updateData
