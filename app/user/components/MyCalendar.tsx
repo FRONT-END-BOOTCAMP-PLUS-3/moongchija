bb464b1993f62442e7e78994dae974a54014a0b0
binwoonki <vinwoonki98@gmail.com>
2025-02-07 16:27:28 +0900

    [feat] MyCalendar 컴포넌트에 로딩 상태에 따른 스켈레톤 UI 적용

--- app/user/components/MyCalendar.tsx
+++ app/user/components/MyCalendar.tsx
@@ -8,6 +8,7 @@ import "./MyCalendar.scss";
 import koLocale from "@fullcalendar/core/locales/ko";
 import { getEventsStatus } from "@/utils/user/getEventsStatus";
 import { Event } from "@/types/Event";
+import CalendarSkeleton from "./user-skeleton/CalendarSkeleton";
 
 const MyCalendar = ({
   onAppointmentsFetch,
@@ -15,9 +16,11 @@ const MyCalendar = ({
   onAppointmentsFetch: (appointments: Event[]) => void;
 }) => {
   const [events, setEvents] = useState<Event[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const appointmentsListFetch = async () => {
+      setIsLoading(true);
       const response = await fetch("/api/user/user-appointments");
       const appointments = await response.json();
       const transformedEvents = appointments.map((event: Event) => {
@@ -32,6 +35,7 @@ const MyCalendar = ({
 
       setEvents(transformedEvents);
       onAppointmentsFetch(transformedEvents);
+      setIsLoading(false);
     };
 
     appointmentsListFetch();
@@ -39,33 +43,37 @@ const MyCalendar = ({
 
   return (
     <div className="calendarWrapper">
-      <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView="dayGridMonth"
-        firstDay={1}
-        events={events}
-        eventContent={(eventInfo) => {
-          return (
-            <div
-              style={{
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
-            >
-              {eventInfo.event.title}
-            </div>
-          );
-        }}
-        locale={koLocale}
-        titleFormat={{ year: "2-digit", month: "long" }}
-        headerToolbar={{
-          left: "prev",
-          center: "title",
-          right: "next",
-        }}
-        fixedWeekCount={false}
-      />
+      {isLoading ? (
+        <CalendarSkeleton />
+      ) : (
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          firstDay={1}
+          events={events}
+          eventContent={(eventInfo) => {
+            return (
+              <div
+                style={{
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                }}
+              >
+                {eventInfo.event.title}
+              </div>
+            );
+          }}
+          locale={koLocale}
+          titleFormat={{ year: "2-digit", month: "long" }}
+          headerToolbar={{
+            left: "prev",
+            center: "title",
+            right: "next",
+          }}
+          fixedWeekCount={false}
+        />
+      )}
     </div>
   );
 };
