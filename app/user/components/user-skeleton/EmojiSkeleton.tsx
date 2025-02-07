99f5f910b22823c26a328a20dea1fa75f7254dbe
binwoonki <vinwoonki98@gmail.com>
2025-02-07 16:29:31 +0900

    [style] 이모지 스켈레톤 컴포넌트 생성 및 스타일링

---
+++ app/user/components/user-skeleton/EmojiSkeleton.tsx
@@ -0,0 +1,6 @@
+import styles from "./EmojiSkeleton.module.scss";
+const EmojiSkeleton = () => {
+  return <div className={styles.emojiSkeletonContainer}></div>;
+};
+
+export default EmojiSkeleton;
