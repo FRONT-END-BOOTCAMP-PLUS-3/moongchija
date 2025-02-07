34a0e9495945345809e375219cbcd452067d893f
binwoonki <vinwoonki98@gmail.com>
2025-02-07 17:06:37 +0900

    [refactor] ##fd8446  -> #을 2번 써서 오류 #하나 삭제

--- utils/user/getEventsStatus.ts
+++ utils/user/getEventsStatus.ts
@@ -1,8 +1,10 @@
 import { Event } from "@/types/Event";
 
 export const getEventsStatus = (event: Event) => {
-  const startTime = event.start;
-  const confirmTime = event.confirm_time ? event.confirm_time : null;
+  const startTime = new Date(event.start);
+  const confirmTime = event.confirmed_time
+    ? new Date(event.confirmed_time)
+    : null;
 
   if (event.status === "voting") {
     return { status: "voting", color: "#64c964" };
@@ -10,7 +12,7 @@ export const getEventsStatus = (event: Event) => {
 
   if (event.status === "confirmed") {
     if (confirmTime && confirmTime < startTime) {
-      return { status: "scheduled", color: "##fd8446" };
+      return { status: "scheduled", color: "#fd8446" };
     }
     return { status: "confirmed", color: "#fd565f" };
   }
