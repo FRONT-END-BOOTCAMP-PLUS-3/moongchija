d8014ff45d1802b414949ad6665d8e4519e4562b
binwoonki <vinwoonki98@gmail.com>
2025-02-07 13:35:07 +0900

    [feat] 사용자 약속 리스트 유즈케이스

--- application/usecases/appointment/DfGetUserAppointmentsUsecase.ts
+++ application/usecases/appointment/DfGetUserAppointmentsUsecase.ts
@@ -1,15 +1,15 @@
 import { SbAppointmentRepository } from "@/infrastructure/repositories/SbAppointmentRepository";
 import { SbMemberRepository } from "@/infrastructure/repositories/SbMemberRepository";
-import { SbUserRepository } from "@/infrastructure/repositories/SbUserRepository";
+
+import { UserAppointmentListDto } from "./dto/UserAppointmentListDto";
 
 export class DfGetUserAppointmentsUsecase {
   constructor(
-    private userRepository: SbUserRepository,
     private appointmentRepository: SbAppointmentRepository,
     private memberRepository: SbMemberRepository
   ) {}
 
-  async execute(userId: string) {
+  async execute(userId: string): Promise<UserAppointmentListDto[]> {
     const members = await this.memberRepository.findByUserId(userId);
 
     const appointmentIds = members.map((m) => m.appointment_id);
@@ -18,11 +18,9 @@ export class DfGetUserAppointmentsUsecase {
 
     const appointments =
       (await this.appointmentRepository.findByIds(appointmentIds)) ?? [];
-    console.log(appointments);
 
     return appointments.map((appointment) => {
       return {
-        id: appointment.id,
         title: appointment.title,
         status: appointment.status,
         start: appointment.start_time,
