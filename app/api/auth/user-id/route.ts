289d88b245613a458a7743ee3f10403b8e9af268
binwoonki <vinwoonki98@gmail.com>
2025-02-10 10:43:35 +0900

    [refactor] 사용하지 않는 request 제거

--- app/api/auth/user-id/route.ts
+++ app/api/auth/user-id/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const cookieStore = cookies();
   const userId = (await cookieStore).get("userId")?.value || null;
 
